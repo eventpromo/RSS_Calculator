2c401ce454fd944df86b6e71fb43a46a47418dcf
MINSK\Siarhei_Huk <eventpromoby@gmail.com>
2018-03-07 19:42:09 +0300

    impliment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expression = initialValue;    
   }
 
-  add(number) {
-    // your implementation
+  add(number) {    
+    this.expression += "+" + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.expression += "-" + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.expression += "*" + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.expression += "/" + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.expression += "**" + number;
+    return this;
   }
+
+  valueOf(){
+    return eval(this.expression); 
+  }    
 }
 
 module.exports = SmartCalculator;
